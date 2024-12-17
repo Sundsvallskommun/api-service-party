cad5154023dcb8339d0baba44bd6027c786aa71e
cheezi0747 <linus.sjolinder@b3.se>
2024-12-17 10:25:16 +0100

    UF-11348: Add Circuitbreakers

--- src/main/java/se/sundsvall/party/integration/citizen/CitizenClient.java
+++ src/main/java/se/sundsvall/party/integration/citizen/CitizenClient.java
@@ -3,12 +3,14 @@ package se.sundsvall.party.integration.citizen;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 import static se.sundsvall.party.integration.citizen.configuration.CitizenConfiguration.CLIENT_ID;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import se.sundsvall.party.integration.citizen.configuration.CitizenConfiguration;
 
 @FeignClient(name = CLIENT_ID, url = "${integration.citizen.url}", configuration = CitizenConfiguration.class)
+@CircuitBreaker(name = CLIENT_ID)
 public interface CitizenClient {
 
 	/**
